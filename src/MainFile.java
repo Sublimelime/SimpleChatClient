7c56f3bc02c15813ee88f6e52757214916459236
NoahNMorton <noahmorton98@gmail.com>
2017-03-08 14:35:40 -0600

    Wrote code to get username

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,7 +1,19 @@
 
+import java.util.InputMismatchException;
+import javax.swing.JOptionPane;
+
 public class MainFile {
 
     public static void main(String[] args) {
-        new ChatFrame("UserName");
+        String username = "";
+        while (username.equals("")) {
+            try {
+                username = JOptionPane.showInputDialog("Please provide a valid username.");
+            } catch (InputMismatchException e) {
+                System.out.println("Username is invalid.");
+            }
+        }
+        //todo code to check if already taken
+        new ChatFrame(username);
     }
 }
